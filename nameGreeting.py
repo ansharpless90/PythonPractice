2b2cd3a8ceb8736a0183f74949d2ac59063dc9bd
ansharpless90 <ansharpless@outlook.com>
2018-12-30 07:38:19 -0500

    added nameGreeting.py

---
+++ nameGreeting.py
@@ -0,0 +1,9 @@
+#Adrian Sharpless
+#Last mod date: 12/30/18
+
+first_name = input("What is your first name? ")
+last_name = input("What is your last name? ")
+
+full_name = first_name + " " + last_name
+
+print("Hello", full_name)
